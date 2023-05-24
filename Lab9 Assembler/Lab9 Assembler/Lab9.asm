202470c4f2dba3d4340ed50353d6631ebd622f73
Riv2oo4 <riv22500@uvg.edu.gt>
2023-05-24 09:06:27 -0600

    Lab

---
+++ Lab9 Assembler/Lab9 Assembler/Lab9.asm
@@ -0,0 +1,28 @@
+;Universidad del Valle de Guatemala
+;Alexis Mesias, 22562
+;Hugo Rivas, 22500
+;Catedrático: Roger 
+;Descripción; Laboratorio 9 Assembler 
+;17/05/2023
+.386
+
+includelib libcmt.lib
+includelib libvcruntime.lib
+includelib libucrt.lib
+includelib legacy_stdio_definitions.lib
+.model flat, C
+
+printf proto c: vararg
+
+
+.data 
+;Variables 
+
+
+
+.code
+main	proc
+		
+
+main endp
+     end
